43dd3e88a7403f4aab46f21bc0aa3c08bf98178c
Judit Johana Morel Arce <juditjohana.morel358@comunidadunir.net>
2024-12-25 21:23:10 +0100

    Subida inicial del proyecto completo

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppComponent, // Componente standalone importado
+  ],
+})
+export class AppModule {}
